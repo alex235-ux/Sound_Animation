ceedd5ba283efa9d8fa0567bbc1b03c3b1a11699
Denis <neptundr@mail.ru>
2021-04-19 11:20:34 +0700

    Initial commit

---
+++ app/src/main/java/com/alexux/alexux/animation/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.alexux.alexux.animation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private var soundPool:soundPool?=null
+    private var soundid1 int = 0
+    private var soundid2 int = 0
+    private var soundid3 int = 0
+    private var soundid4 int = 0
+    private var soundid5 int = 0
+    private var soundid6 int = 0
+
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
